3cb265a618ba74460eb0d77f086e17916f66aebe
Richard Kish <richard.kish1009@gmail.com>
2024-04-14 16:35:37 -0400

    Add files via upload

---
+++ Sentiment_Analysis_Demo/SentimentAnalysis.R
@@ -0,0 +1,142 @@
+library(tidyverse)
+library(tidytext)
+library(tidyr)
+library(scales)
+library(janeaustenr)
+library(gutenbergr)
+library(wordcloud)
+library(reshape2)
+
+### DATA PULL FROM GUTENBERG LIBRARY ###
+# Jane Austen Library
+jane_austen <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, 
+                                     regex("^chapter [\\divxlc]",
+                                           ignore_case = TRUE)))) %>%
+  ungroup()
+
+# Tokenize all rows, (split sentences into rows, 1 word each)
+# Remove stop words (common words, not needed in sentiment analysis)
+tidy_jane <- jane_austen %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+# Count frequency of each word
+tidy_jane %>%
+  count(word, sort = TRUE)
+
+# Bronte Sisters Library 
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
+
+tidy_bronte <- bronte %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+tidy_bronte %>%
+  count(word, sort = TRUE)
+
+# HG Wells Library
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+
+tidy_hgwells <- hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+tidy_hgwells %>%
+  count(word, sort = TRUE)
+
+# Calculate word frequencies for all books combined into one dataset
+frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"), 
+                       mutate(tidy_jane, author = "Jane Austen")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  pivot_wider(names_from = author, values_from = proportion) %>%
+  pivot_longer(`Brontë Sisters`:`H.G. Wells`,
+               names_to = "author", values_to = "proportion")
+
+# Word frequencies in Jane Austen Library compared to Bronte Sisters
+# Library, and HG Wells Library (Visualization)
+ggplot(frequency, aes(x = proportion, y = `Jane Austen`, 
+                      color = abs(`Jane Austen` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(limits = c(0, 0.001), 
+                       low = "darkslategray4", high = "gray75") +
+  facet_wrap(~author, ncol = 2) +
+  theme(legend.position="none") +
+  labs(y = "Jane Austen", x = NULL)
+
+# Correlation data for Jane Austen library vs Bronte & HG Wells
+jane_vs_bronte <- cor.test(data = frequency[frequency$author == "Brontë Sisters",],
+         ~ proportion + `Jane Austen`)
+
+jane_vs_wells <- cor.test(data = frequency[frequency$author == "H.G. Wells",], 
+                          ~ proportion + `Jane Austen`)
+
+### SENTIMENT ANALYSIS ###
+# Get sentiments dictionary
+bing_dict <- get_sentiments("bing")
+
+# Join Jane Austen Library with bing sentiment dictionary
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(
+    linenumber = row_number(),
+    chapter = cumsum(str_detect(text, 
+                                regex("^chapter [\\divxlc]", 
+                                      ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+# Find sentiment of each line and word in Jane Austen library
+jane_austen_sentiment <- tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(book, index = linenumber %/% 80, sentiment) %>%
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+
+# Visualize sentiment for all Jane Austen books
+ggplot(jane_austen_sentiment, aes(index, sentiment, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~book, ncol = 2, scales = "free_x")
+
+# Calculate the top positive/negative words in the library
+bing_word_counts <- tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+# Plot the top 10 positive/negative words found
+bing_word_counts %>%
+  group_by(sentiment) %>%
+  slice_max(n, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+
+# Word Cloud based on word frequency
+tidy_books %>%
+  anti_join(stop_words) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+# Word Cloud based on sentiment for top words
+tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("red3", "green4"),
+                   max.words = 100)
